45a3d720e73b6d09dc25ae1c606ac245295c519c
Robin Cornelius <robin.cornelius@gmail.com>
2015-10-24 22:07:50 +0100

    Stub out all other data files with factory creators

---
+++ libECRComms/DataFiles/Group.cs
@@ -0,0 +1,74 @@
+﻿/*
+
+    This file is part of ECRComms.
+
+    ECRComms is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ECRComms is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ECRComms.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright (c) 2013 - 2015 Robin Cornelius <robin.cornelius@gmail.com>
+ */
+
+// ******************* PROGRAM 1 Group Data ************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace libECRComms.DataFiles
+{
+    class GroupDataFactory
+    {
+        public static GroupData Get(MachineIDs id)
+        {
+            switch (id)
+            {
+                case MachineIDs.ER230:
+                case MachineIDs.ER380M_UK:
+                    return new GroupData1();
+
+                default:
+                    return null;
+
+            }
+
+        }
+
+    }
+
+    public abstract class GroupData : data_serialisation
+    {
+
+        public GroupData()
+        {
+
+        }
+
+        public override void decode()
+        {
+        }
+
+        public override void encode()
+        {
+        }
+    }
+
+
+     public class GroupData1 : GroupData
+     {
+        public GroupData1()
+        {
+
+        }
+     }
+}
---
+++ libECRComms/DataFiles/SystemOptions.cs
@@ -0,0 +1,74 @@
+﻿/*
+
+    This file is part of ECRComms.
+
+    ECRComms is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ECRComms is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ECRComms.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright (c) 2013 - 2015 Robin Cornelius <robin.cornelius@gmail.com>
+ */
+
+// ******************* PROGRAM 3 System Data ************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace libECRComms.DataFiles
+{
+    class SystemDataFactory
+    {
+        public static SystemData Get(MachineIDs id)
+        {
+            switch (id)
+            {
+                case MachineIDs.ER230:
+                case MachineIDs.ER380M_UK:
+                    return new SystemData1();
+
+                default:
+                    return null;
+
+            }
+
+        }
+
+    }
+
+    public abstract class SystemData : data_serialisation
+    {
+
+        public SystemData()
+        {
+
+        }
+
+        public override void decode()
+        {
+        }
+
+        public override void encode()
+        {
+        }
+    }
+
+
+    public class SystemData1 : SystemData
+    {
+        public SystemData1()
+        {
+
+        }
+    }
+}
